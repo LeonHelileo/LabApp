d90b4642d847e802a02209d0d14e67366e11e558
Leonid Gneiting <leonid.gneiting@gmail.com>
2022-07-07 11:51:36 +0200

    Closure View added

---
+++ LabApp/ClosuresViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClosuresViewController.swift
+//  LabApp
+//
+//  Created by Leonid Gneiting on 07/07/2022.
+//
+
+import UIKit
+
+class ClosuresViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
